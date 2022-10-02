c07af14c5e8b6dc330cf335c216d126de9a150e7
Fatwah Firmansyah <fatwah091@gmail.com>
2022-10-02 11:50:59 +0700

    Menambahkan menu pada sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -24,83 +24,70 @@
                 </a>
             </li>
             <li>
+                <hr class="mt-4">
+            </li>
+            <li>
                 <a href="#" class="main-menu has-dropdown">
                     <i class="ti-desktop"></i>
-                    <span>UI Elements</span>
+                    <span>Gizi</span>
                 </a>
                 <ul class="sub-menu ">
-                    <li><a href="element-ui.html" class="link"><span>Elements</span></a></li>
-                    <li><a href="element-accordion.html" class="link"><span>Accordion</span></a></li>
-                    <li><a href="element-tabs-collapse.html" class="link"><span>Tabs & Collapse</span></a></li>
-                    <li><a href="element-card.html" class="link"><span>Card</span></a></li>
-                    <li><a href="element-button.html" class="link"><span>Buttons</span></a></li>
-                    <li><a href="element-alert.html" class="link"><span>Alert</span></a></li>
-                    <li><a href="element-themify-icons.html" class="link"><span>Themify Icons</span></a></li>
-                    <li><a href="element-modal.html" class="link"><span>Modal</span></a></li>
+                    <li><a href="element-ui.html" class="link"><span>Data Pemeriksaan Gizi</span></a></li>
+                    <li><a href="element-ui.html" class="link"><span>Riwayat</span></a></li>
                 </ul>
             </li>
             <li>
                 <a href="#" class="main-menu has-dropdown">
-                    <i class="ti-book"></i>
-                    <span>Form</span>
+                    <i class="ti-desktop"></i>
+                    <span>Imunisasi</span>
                 </a>
                 <ul class="sub-menu ">
-                    <li><a href="form-element.html" class="link">
-                            <span>Form Element</span></a>
-                    </li>
-                    <li><a href="form-datepicker.html" class="link">
-                            <span>Datepicker</span></a>
-                    </li>
-                    <li><a href="form-select2.html" class="link">
-                            <span>Select2</span></a>
-                    </li>
+                    <li><a href="element-ui.html" class="link"><span>Data Imunisasi</span></a></li>
+                    <li><a href="element-ui.html" class="link"><span>Jenis Imunisasi</span></a></li>
                 </ul>
             </li>
             <li>
-                <a href="#" class="main-menu has-dropdown">
-                    <i class="ti-notepad"></i>
-                    <span>Utilities</span>
-                </a>
-                <ul class="sub-menu">
-                    <li><a href="error-404.html" target="_blank" class="link"><span>Error 404</span></a></li>
-                    <li><a href="error-403.html" target="_blank" class="link"><span>Error 403</span></a></li>
-                    <li><a href="error-500.html" target="_blank" class="link"><span>Error 500</span></a></li>
-                </ul>
+                <hr class="mt-4">
             </li>
             <li>
                 <a href="#" class="main-menu has-dropdown">
-                    <i class="ti-layers-alt"></i>
-                    <span>Pages</span>
+                    <i class="ti-desktop"></i>
+                    <span>Unit Kesehatan</span>
                 </a>
                 <ul class="sub-menu ">
-                    <li><a href="pages-blank.html" class="link"><span>Blank</span></a></li>
+                    <li><a href="element-ui.html" class="link"><span>Puskesmas</span></a></li>
+                    <li><a href="element-ui.html" class="link"><span>Posyandu</span></a></li>
                 </ul>
             </li>
             <li>
                 <a href="#" class="main-menu has-dropdown">
-                    <i class="ti-hummer"></i>
-                    <span>Auth</span>
-                </a>
-                <ul class="sub-menu">
-                    <li><a href="auth-login.html" target="_blank" class="link"><span>Login</span></a></li>
-                    <li><a href="auth-register.html" target="_blank" class="link"><span>Register</span></a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#" class="main-menu has-dropdown">
-                    <i class="ti-write"></i>
-                    <span>Tables</span>
+                    <i class="ti-desktop"></i>
+                    <span>Data Wilayah</span>
                 </a>
                 <ul class="sub-menu ">
-                    <li><a href="table-basic.html" class="link"><span>Table Basic</span></a></li>
-                    <li><a href="table-datatables.html" class="link"><span>DataTables</span></a></li>
+                    <li><a href="element-ui.html" class="link"><span>Kecamatan</span></a></li>
+                    <li><a href="element-ui.html" class="link"><span>Desa</span></a></li>
                 </ul>
             </li>
             <li>
-                <a href="charts.html" class="link">
-                    <i class="ti-bar-chart"></i>
-                    <span>Charts</span>
+                <a href="#" class="main-menu has-dropdown">
+                    <i class="ti-desktop"></i>
+                    <span>Akun Pengguna</span>
                 </a>
+                <ul class="sub-menu ">
+                    <li><a href="element-ui.html" class="link"><span>Akun Bidan</span></a></li>
+                    <li><a href="element-ui.html" class="link"><span>Akun Puskesmas</span></a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" class="main-menu has-dropdown">
+                    <i class="ti-desktop"></i>
+                    <span>Pasien</span>
+                </a>
+                <ul class="sub-menu ">
+                    <li><a href="element-ui.html" class="link"><span>Data Pasien</span></a></li>
+                    <li><a href="element-ui.html" class="link"><span>Data Keluarga</span></a></li>
+                </ul>
             </li>
         </ul>
     </div>
